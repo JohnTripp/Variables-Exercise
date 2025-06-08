e0ac17e0546ca0d68e2b54ee3daba9cb04ba3d52
John Tripp <madfish13@hotmail.com>
2025-06-08 04:22:45 -0400

    Done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Variables
+            //camelCase for variable name - everyOtherLetterStartsWithCapital
+
+            //Number types
+
+            //Declaring a variable (creation of a variable)
+            int wholeNumber;
+
+            //Initializing a variable (giving a variable a value)
+            wholeNumber = 5;
+
+            //Or do both
+            //Declaring and initializing
+            int newNumber = 10;
+
+            double average = 99.80;
+
+            decimal bankAccount = 1000.00m;
+
+            //Word types
+
+            string ourClass = "CSharp-61";
+
+            char initial = 'S';
+
+            //Fact type
+
+            bool isCoding = true;
+
+            //String interpolation
+
+            //string message = $"Our class is {ourClass}";
+
+            Console.WriteLine($"Our class is {ourClass}");
+
+            //Take user input
+
+            Console.WriteLine("Give me your name;");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userName}!");
+
+            string myName = "John";
+
+            int age = 25;
+            
+            bool isMarried = true;
+                
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
         }
     }
 }
